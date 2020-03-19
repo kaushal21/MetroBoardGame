4ff1e429b408d243c0d40690de549f7233e17cd2
Tom Stephens <@u6953044@anu.edu.au>
2020-03-19 12:01:38 +1100

    Added Board class and some ideas for methods it might use

---
+++ src/comp1110/ass2/Board.java
@@ -0,0 +1,15 @@
+package comp1110.ass2;
+
+public class Board extends Metro {
+    public static void initializeBoard() {
+
+    }
+
+    public static void allocateStations() {
+
+    }
+
+    public static void updateBoardState() {
+
+    }
+}
