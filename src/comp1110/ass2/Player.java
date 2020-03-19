f38b26a862d826f478c77af12e53357e03d6e285
Tom Stephens <@u6953044@anu.edu.au>
2020-03-19 12:11:11 +1100

    Added Player class and some ideas for methods it might use

---
+++ src/comp1110/ass2/Player.java
@@ -0,0 +1,12 @@
+package comp1110.ass2;
+
+public class Player {
+    int players = 0;
+
+    public static void switchTurn() {
+
+    }
+    public static void canPickUpTile() {
+        
+    }
+}
