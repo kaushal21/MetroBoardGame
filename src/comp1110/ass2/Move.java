7753721d4e7e7d55b462575db11a969f9ab4f268
Tom Stephens <@u6953044@anu.edu.au>
2020-03-19 12:05:32 +1100

    Added Move class and some ideas for methods it might use

---
+++ src/comp1110/ass2/Move.java
@@ -0,0 +1,13 @@
+package comp1110.ass2;
+
+public class Move {
+    public static void drawFromDeck() {
+
+    }
+    public static void updateTilePlacement() {
+
+    }
+    public static void updatePlacementSequence() {
+        
+    }
+}
