d51ae09e5ac374d3abef2f4fb4718ccdc65def0a
Tom Stephens <@u6953044@anu.edu.au>
2020-05-03 18:20:02 +1000

    Created class to hold our tests

---
+++ tests/comp1110/ass2/OurTests.java
@@ -0,0 +1,30 @@
+package comp1110.ass2;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class OurTests {
+    @Rule
+    public Timeout globalTimeout = Timeout.millis(1000);
+
+    // Tests the situation where a track goes directly between 2 adjacent corner stations or 2 adjacent edge stations
+    // It also includes a track that goes to the middle station to check if that works (although that may be part of another test)
+    @Test
+    public void testPlayerScoreHelper() {
+        String[] tileArray = {"cbcb00","bcbc07","bcbc70","dbcd77","ccda73","cbaa74","cbaa64","cbaa54"};
+        int[] expected = {7};
+        int[] outcome = {Score.scorePlayer(2,tileArray, 6)};
+        test(expected, outcome);
+    }
+
+    private void test(int[] expected, int[] out) {
+        assertNotNull("Expected score to be " + Arrays.toString(expected) + ", but got null", out);
+        assertTrue("Expected score to be " + Arrays.toString(expected) + ", but got " + Arrays.toString(out), Arrays.equals(expected, out));
+    }
+
+}
