79a7cd12e54eedb33f39d7ed1ff569c9f2cde3e9
Tom Stephens <@u6953044@anu.edu.au>
2020-05-03 18:19:20 +1000

    Moved my test to OurTests.java

--- tests/comp1110/ass2/GetScoreTest.java
+++ tests/comp1110/ass2/GetScoreTest.java
@@ -40,16 +40,6 @@ public class GetScoreTest {
         }
     }
 
-    // Tests the situation where a track goes directly between 2 adjacent corner stations or 2 adjacent edge stations
-    // It also includes a track that goes to the middle station to check if that works (although that may be part of another test)
-    @Test
-    public void testPlayerScoreHelper() {
-        String[] tileArray = {"cbcb00","bcbc07","bcbc70","dbcd77","ccda73","cbaa74","cbaa64","cbaa54"};
-        int[] expected = {7};
-        int[] outcome = {Score.scorePlayer(2,tileArray, 6)};
-        test(expected, outcome);
-    }
-
     private void test(int[] expected, int[] out) {
         assertNotNull("Expected score to be " + Arrays.toString(expected) + ", but got null", out);
         assertTrue("Expected score to be " + Arrays.toString(expected) + ", but got " + Arrays.toString(out), Arrays.equals(expected, out));
