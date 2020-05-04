5f49bf4d423378adfe53f8445f6acc676195256e
Kaushal <kaushal.sharma@anu.edu.au>
2020-05-04 19:47:40 +1000

    Updated Some JUnit Tests and E-newcode-u7028766.md

--- admin/E-newcode-u7028766.md
+++ admin/E-newcode-u7028766.md
@@ -5,6 +5,6 @@
 For Deliverable D2E, I contributed the following new statements of original code:
 
 - Move.java - line 154-433
-- OurTests.java - line 39-66
+- OurTests.java - line 55-101
 
 (List at least 20, including at least one new unit test, and include line numbers.)
--- tests/comp1110/ass2/OurTests.java
+++ tests/comp1110/ass2/OurTests.java
@@ -55,11 +55,30 @@ public class OurTests {
     // This test is to check if the top tile is popped out or not
     @Test
     public void testPop() {
-        Tile tile = new Tile();
-        String topDeck = tile.getDeck(tile.getTop()-1);
-        String poppedTile = tile.pop();
-        assertNotNull("Expected popped tile to be "+topDeck+" but got NULL",poppedTile);
-        assertEquals("Expected Tile: " + topDeck + " But Got This: " + poppedTile, topDeck, poppedTile);
+        for(int i = 0; i < Utilities.VALID_SMALL_BOARDSTRINGS.length; i++) {
+            Tile tile = new Tile();
+            for(int j = 0; j < Utilities.VALID_SMALL_BOARDSTRINGS[i].length() / 6; j++) {
+                String placedTiles = Utilities.VALID_SMALL_BOARDSTRINGS[i].substring(j*6, (j*6)+6);
+                placedTiles = placedTiles.substring(0, 4);
+                tile.updateDeck(placedTiles);
+            }
+            String topDeck = tile.getDeck(tile.getTop()-1);
+            String poppedTile = tile.pop();
+            assertNotNull("Expected popped tile to be "+topDeck+" but got NULL",poppedTile);
+            assertEquals("Expected Tile: " + topDeck + " But Got This: " + poppedTile, topDeck, poppedTile);
+        }
+        for(int i = 0; i < Utilities.VALID_MEDIUM_BOARDSTRINGS.length; i++) {
+            Tile tile = new Tile();
+            for(int j = 0; j < Utilities.VALID_MEDIUM_BOARDSTRINGS[i].length() / 6; j++) {
+                String placedTiles = Utilities.VALID_MEDIUM_BOARDSTRINGS[i].substring(j*6, (j*6)+6);
+                placedTiles = placedTiles.substring(0, 4);
+                tile.updateDeck(placedTiles);
+            }
+            String topDeck = tile.getDeck(tile.getTop()-1);
+            String poppedTile = tile.pop();
+            assertNotNull("Expected popped tile to be "+topDeck+" but got NULL",poppedTile);
+            assertEquals("Expected Tile: " + topDeck + " But Got This: " + poppedTile, topDeck, poppedTile);
+        }
     }
 
     // This test is to check if the deck is Empty or not
@@ -71,12 +90,13 @@ public class OurTests {
 
         // Check for empty deck
         for(int i = 0; i < Utilities.COMPLETE_BOARDSTRINGS.length; i++) {
+            Tile lastTile = new Tile();
             for(int j = 0; j < Utilities.COMPLETE_BOARDSTRINGS[i].length() / 6; j++) {
                 String placedTiles = Utilities.COMPLETE_BOARDSTRINGS[i].substring(j*6, (j*6)+6);
                 placedTiles = placedTiles.substring(0, 4);
-                tile.updateDeck(placedTiles);
+                lastTile.updateDeck(placedTiles);
             }
-            assertFalse("Expected false but got "+tile.checkDeckEmpty(), tile.checkDeckEmpty());
+            assertFalse("Expected false but got "+lastTile.checkDeckEmpty(), lastTile.checkDeckEmpty());
         }
     }
 }
