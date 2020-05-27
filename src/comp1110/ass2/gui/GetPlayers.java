ec1bd29f3b52c1041ac48f0509c34f99631fc124
Tom Stephens <@u6953044@anu.edu.au>
2020-05-27 17:15:11 +1000

    updated to also get the number of AI the player wants in the game

--- src/comp1110/ass2/gui/GetPlayers.java
+++ src/comp1110/ass2/gui/GetPlayers.java
@@ -12,6 +12,8 @@ import javafx.stage.Stage;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+// Authorship: Kaushal Sharma, Tom Stephens
+
 public class GetPlayers {
 
     /**
@@ -19,8 +21,9 @@ public class GetPlayers {
      * @return The integer value accepted from the user.
      */
     public static int getPlayers() {
-        // This stores the number of players entered by the user.
-        AtomicInteger n = new AtomicInteger();
+        // These store the total number of players entered by the user, and the number of computer opponents
+        AtomicInteger h = new AtomicInteger();
+        AtomicInteger a = new AtomicInteger();
         // Create a new Stage to Accept the Number of Players
         Stage newWindow = new Stage();
 
@@ -32,28 +35,34 @@ public class GetPlayers {
             e.consume();
             String mess = "Are you sure you want to quit?!!";
             if ( ConfirmationBox.confirmationBox(mess) ) {
-                n.set(-1);
+                h.set(-1);
                 newWindow.close();
             }
         });
 
-        Label name = new Label("Enter The number of Players (2-6):");       // Label Used to display the message
+        Label noOfPlayers = new Label("Enter The number of Players (2-6):");       // Label Used to display the message
         TextField numberOfPlayers = new TextField();                            // TextField is used to store the input value
         numberOfPlayers.setMaxWidth(100);
         numberOfPlayers.setPromptText("No. of Players");
+        Label noOfAIs = new Label("Enter The number of Computer opponents (must be less than the number of players):");
+        TextField numberOfAIs = new TextField();
+        numberOfAIs.setMaxWidth(100);
+        numberOfAIs.setPromptText("No. of Computer opponents");
+
         Button submit = new Button("PLAY");                               // Create a Play button
         submit.setOnAction(e -> {
             // On click functionality of submit button
             // Check if the value entered is correct
-            if ( checkEnteredNumberOfPlayers(numberOfPlayers) ) {
-                n.set(Integer.parseInt(numberOfPlayers.getText()));
+            if (checkEnteredNumberOfPlayers(numberOfPlayers) && checkEnteredNumberOfAIs(numberOfPlayers,numberOfAIs) ) {
+                h.set(Integer.parseInt(numberOfPlayers.getText()));
+                a.set(Integer.parseInt(numberOfAIs.getText()));
                 newWindow.close();
             }
         });
 
         // Create a VBox to display this layout on the screen
         VBox layout = new VBox(10);
-        layout.getChildren().addAll(name, numberOfPlayers, submit);
+        layout.getChildren().addAll(noOfPlayers, numberOfPlayers, noOfAIs, numberOfAIs, submit);
         layout.setAlignment(Pos.CENTER);
         layout.setPadding(new Insets(5, 10, 5, 10));
 
@@ -62,14 +71,14 @@ public class GetPlayers {
         newWindow.showAndWait();
 
         // return the entered value
-        return n.get();
+        return h.get();
     }
 
     /**
      * It called by the submit button while accepting the number of users. It checks 2 conditions
      * 1.> The entered number of players is in integer.
      * 2.> The entered number is in the range from 2 to 6.
-     * @param number It is the TextField which have the value enterd from the user
+     * @param number It is the TextField which have the value entered from the user
      * @return true if the entered value is valid as number of players, otherwise it returns false
      */
     static boolean checkEnteredNumberOfPlayers(TextField number) {
@@ -86,6 +95,32 @@ public class GetPlayers {
         } catch (NumberFormatException e1) {
             // Print out the error message and return false
             System.out.println("Enter the number of players correctly");
+            return false;
+        }
+    }
+    /**
+     * It called by the submit button while accepting the number of users. It checks 2 conditions
+     * 1.> The entered number of players is in integer.
+     * 2.> The entered number is not more than the total number of players - 1 (since we want at least 1 human player)
+     * @param totalNumber It is the TextField which have the total no. of players value entered from the user
+     * @param number It is the TextField which have the no. of computer opponents value entered from the user
+     * @return true if the entered value is valid as number of computer opponents, otherwise it returns false
+     */
+    static boolean checkEnteredNumberOfAIs(TextField totalNumber, TextField number) {
+        try {
+            // Convert the entered TextField value to Integer
+            int totalNum = Integer.parseInt(totalNumber.getText());
+            int numAI = Integer.parseInt(number.getText());
+            // Check the range of the entered value
+            if ( numAI < 0 || (numAI >= totalNum) )
+                // If the value is out of range then throw an exception.
+                throw new NumberFormatException("You can't have more opponents than players!");
+            else
+                // If the value is within the range then return true.
+                return true;
+        } catch (NumberFormatException e1) {
+            // Print out the error message and return false
+            System.out.println("Enter the number of AIs correctly");
             return false;
         }
     }
