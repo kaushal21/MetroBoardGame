a7bc336e536b6e42cc0d46a1b3dd47c5952b3768
Tom Stephens <@u6953044@anu.edu.au>
2020-05-27 15:22:36 +1000

    Fixing merge conflict

---
+++ src/comp1110/ass2/OpponentAI.java
@@ -0,0 +1,19 @@
+package comp1110.ass2;
+
+//
+
+
+// make extend Player class??
+public class OpponentAI {
+
+    /**
+     * The number of AIs playing the game is taken as an input from the user (between 1 and 5).
+     */
+    public OpponentAI(int n) {
+        int opponents = n;
+        String[] opponentsTile1 = new String[opponents];
+
+    }
+
+
+}
