87d630c8dfb6cb60f9e2ec29a901e5f945fc491c
NanoA <u6671730@anu.edu.au>
2020-05-28 23:37:29 +1000

    Got Score Working on the Screen

--- src/comp1110/ass2/gui/FinalScreen.java
+++ src/comp1110/ass2/gui/FinalScreen.java
@@ -8,11 +8,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class FinalScreen {
-    public static void finalScreen(String placement) {
+    public static void finalScreen(String placement, int numOfPlayers) {
         Stage newWindow = new Stage();
 
         //Makes its compulsory window
@@ -31,6 +33,22 @@ public class FinalScreen {
         layout.getChildren().addAll(winner1, winner2,score,finishGame);           //Adds the elements to the screen
         layout.setAlignment(Pos.CENTER);                        //Sets the alignment for layout
         layout.setPadding(new Insets(5,10,5,10));
+
+        int i = 1;
+        while (i <= numOfPlayers){
+            Label playerScore1 = new Label();
+            playerScore1.setText("Player " + i + "'s Score: " +  "\n");// Number of player not current player;
+            System.out.println("Player " + i);
+            playerScore1.setMinWidth(250);                                          // Set Minimum Width
+            playerScore1.setAlignment(Pos.CENTER_LEFT);                             // Set Alignment to Center
+            playerScore1.setFont(Font.font("Arial", 15));                     // Set Font of the Text
+            playerScore1.setTextFill(Color.BLACK);                                  // Set Color of the Text
+            playerScore1.setPadding(new Insets(0, 0, 10, 0));         // Set Padding for the Label
+            //playerScore1.setLayoutX((SQUARE_SIZE * 10) + (12));
+            playerScore1.setLayoutY(100+ (i*25));
+            layout.getChildren().add(playerScore1);
+            i++;
+        }
 
         Scene newScene = new Scene(layout,250,100);
         newWindow.setScene(newScene);
