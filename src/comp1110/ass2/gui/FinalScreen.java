0473254e6bc71baa3c83489c5971d379e2781b4d
NanoA <u6671730@anu.edu.au>
2020-05-29 05:31:36 +1000

    Added Comments

--- src/comp1110/ass2/gui/FinalScreen.java
+++ src/comp1110/ass2/gui/FinalScreen.java
@@ -27,18 +27,17 @@ public class FinalScreen{
         newWindow.setTitle("Finish Game");                      //Setting title for the window
         newWindow.setMinWidth(250);                             //Setting the min width of the window
         newWindow.setMinHeight(500);
-
+        //Finding the max number in the array.
         int[] scores2 = Metro.getScore(placement,numOfPlayers);
-        int largest = scores2[0],index = 0;
-            for (int i = 1; i < scores2.length; i++){
-                if(scores2[i] >= largest){
+        int largest = scores2[0], index = 0;
+            for (int i = 1; i < scores2.length; i++){ //Looks through the array.
+                if(scores2[i] >= largest){ //Check against every number to find the largest.
                     largest = scores2[i];
-                    index = i;
+                    index = i; //Sets the location of the largest number
                 }
 
             }
-        System.out.println("WOW: " + index);
-
+        //Setting design elements of the labels.
         Label winner1 = new Label("The Winner is: ");
         winner1.setFont(Font.font("Arial", 15));
         winner1.setTextFill(Color.BLACK);
@@ -48,6 +47,7 @@ public class FinalScreen{
         Label score = new Label();
         Button finishGame = new Button("Finish Game");       //Create a Finish/Close button
         finishGame.setOnAction(e ->newWindow.close());
+        //Displaying who is the winner by finding the max score in the array.
         switch (index){
             case 0:
                 winner2.setText("Player 1");
@@ -73,20 +73,21 @@ public class FinalScreen{
         layout.getChildren().addAll(winner1, winner2,score,finishGame);           //Adds the elements to the screen
         layout.setAlignment(Pos.CENTER);                        //Sets the alignment for layout
         layout.setPadding(new Insets(5,10,5,10));
-
+        //Displaying the correct number of players and their score.
         int i = 0;
         while (i < numOfPlayers){
             int[] scores = Metro.getScore(placement,numOfPlayers);
+            //Calling the label playerScore1 to the amount of players.
             Label playerScore1 = new Label();
-            playerScore1.setText("Player " + (i+1) + "'s Score: " + scores[i] + "\n");// Number of player not current player;
-            System.out.println("Player " + i);
+            //Setting the player number and score line after line.
+            playerScore1.setText("Player " + (i+1) + "'s Score: " + scores[i] + "\n");
             playerScore1.setMinWidth(250);                                          // Set Minimum Width
             playerScore1.setAlignment(Pos.CENTER_LEFT);                             // Set Alignment to Center
             playerScore1.setFont(Font.font("Arial", 15));                     // Set Font of the Text
             playerScore1.setTextFill(Color.BLACK);                                  // Set Color of the Text
             playerScore1.setPadding(new Insets(0, 0, 10, 0));         // Set Padding for the Label
-            playerScore1.setLayoutY(100+ (i*25));
-            layout.getChildren().add(playerScore1);
+            playerScore1.setLayoutY(100+ (i*25)); // Changing the position so they do not overlap.
+            layout.getChildren().add(playerScore1); //Adding the label to the screen.
             i++;
         }
 
